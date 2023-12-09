6d3001d284c37b6be1ff00583d692118ee279b0a
Shraddha Agrawal <shraddha.agrawal000@gmail.com>
2023-12-09 21:55:47 +0530

    add solution for day9

---
+++ library/string.go
@@ -0,0 +1,47 @@
+package aoc
+
+import (
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+func FetchSliceOfIntsInString(line string) []int {
+	nums := []int{}
+	var build strings.Builder
+	isNegative := false
+	for _, char := range line {
+		if unicode.IsDigit(char) {
+			build.WriteRune(char)
+		}
+
+		if char == '-' {
+			isNegative = true
+		}
+
+		if char == ' ' && build.Len() != 0 {
+			localNum, err := strconv.ParseInt(build.String(), 10, 64)
+			if err != nil {
+				panic(err)
+			}
+			if isNegative {
+				localNum *= -1
+			}
+			nums = append(nums, int(localNum))
+			build.Reset()
+			isNegative = false
+		}
+	}
+	if build.Len() != 0 {
+		localNum, err := strconv.ParseInt(build.String(), 10, 64)
+		if err != nil {
+			panic(err)
+		}
+		if isNegative {
+			localNum *= -1
+		}
+		nums = append(nums, int(localNum))
+		build.Reset()
+	}
+	return nums
+}
